d658ceda5e6f6f020fea7e1443d236a22449a9f0
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2024-04-19 22:16:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-//your JS code here. If required.
-// Assuming you have the correct selector now (.btn-close)
-cy.get('#signupModal').should('be.visible'); // Check if the modal is visible
-cy.get('.btn-close').click();               // Attempt to close the modal
-cy.get('#signupModal').should('not.be.visible'); // Assert the modal is not visible
-
-
-cy.get('.btn-close').click();
-cy.wait(500); // wait for 500ms
-cy.get('#signupModal').should('not.be.visible');
+cy.get('.close').click({ force: true })
